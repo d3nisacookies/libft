fb92174be3076ec36405cd813ee620ab09e7c2f3
d3nisacookies <131571593+d3nisacookies@users.noreply.github.com>
2025-09-28 07:53:52 +0000

    codespace testing

---
+++ ft_memmove.c
@@ -0,0 +1,12 @@
+#include <libft.h>
+
+void *ft_memmove(void *dest, const void *src, size_t len)
+{
+    unsigned char *dest_tmp;
+    unsigned char *src_tmp;
+
+    dest_tmp = (unsigned char *)dest;
+    src_tmp = (unsigned char *)src;
+
+    
+}
--- libft.h
+++ libft.h
@@ -1,15 +1,22 @@
-#ifndef FTLIB_H
-# define FTLIB_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
 
+//character checks
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isascii(int c);
 int ft_isalnum(int c);
 int ft_isprint(int c);
 
+//string functions
 size_t ft_strlen(const char *c);
+void ft_bzero(void *s, size_t n);
+void *ft_memcpy(void *dest, const void *s, size_t n);
+void *ft_memmove(void *dest, const void *s, size_t len);
+void *ft_memset(void *s, int c, size_t len);
 
 #endif
