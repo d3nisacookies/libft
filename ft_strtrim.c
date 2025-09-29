c6c0579a3625228c192857b8c049b6abde2a3b55
d3nisacookies <131571593+d3nisacookies@users.noreply.github.com>
2025-09-29 12:38:40 +0000

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,57 @@
+#include "libft.h"
+
+static int to_trim(char *set, char c)
+{
+    size_t i;
+    i = 0;
+
+    while(set[i])
+    {
+        if (c == set[i])
+            return 1;
+        i++;
+    }
+    return 0;
+}
+
+static const char *new_str(const char *s1, size_t start, size_t len)
+{
+    char *str;
+    size_t i;
+
+    if(len <= 0 || start >= ft_strlen(s1))
+    {
+        return(ft_strdup(""));
+    }
+    str = ft_calloc(len + 1, sizeof(char));
+    if (!str)
+    {
+        return NULL;
+    }
+    i = 0;
+    while(i < len)
+    {
+        str[i] = s1[start + i];
+        i++;
+    }
+    return (str);
+}
+
+char *ft_strtrim(const char *s1, const char *set)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+    j = ft_strlen(s1) - 1;
+
+    if (ft_strlen(s1) == 0)
+    {
+        return (ft_strdup(""));
+    }
+    while (to_trim(set, s1[i]))
+        i++;
+    while (to_trim(set, s1[j]))
+        j--;
+    return(new_str(s1, i, j - (i - 1)));
+}
